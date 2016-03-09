e6f3100fc096b59e83da5380bac9d5a5839705ad
Brian Dean Jennings <bjennings@magicleap.com>
2016-03-10 11:06:47 +1300

    Better error handling of show/hide of deleted objects

--- Assets/Plugins/Isolationist/IsolateInfo.cs
+++ Assets/Plugins/Isolationist/IsolateInfo.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Plugins.Isolationist
@@ -12,8 +13,8 @@ namespace Plugins.Isolationist
 		public static IsolateInfo Instance { get { return _instance ? _instance : (_instance = FindObjectOfType<IsolateInfo>()); } set { _instance = value; } }
 		public static bool IsIsolated { get { return Instance; } }
 
-		public static void Hide() { if (Instance && Instance.HiddenObjects != null) Instance.HiddenObjects.ForEach(go => go.SetActive(false)); }
-		public static void Show() { if (Instance && Instance.HiddenObjects != null) Instance.HiddenObjects.ForEach(go => go.SetActive(true)); }
+		public static void Hide() { if (Instance && Instance.HiddenObjects != null) Instance.HiddenObjects.Where(go => go).ToList().ForEach(go => go.SetActive(false)); }
+		public static void Show() { if (Instance && Instance.HiddenObjects != null) Instance.HiddenObjects.Where(go => go).ToList().ForEach(go => go.SetActive(true)); }
 
 		private void Awake() { Show(); }
 	}
